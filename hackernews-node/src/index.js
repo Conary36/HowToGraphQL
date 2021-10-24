dba5a3f3e19505128a6824845564228cd477ce53
Conary36 <crossingwaters@icloud.com>
2021-10-24 12:01:50 -0400

    First commit

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,23 @@
+const {ApolloServer} = require('apollo-server');
+
+const typeDefs = `
+    type Query {
+        info: String!
+    }
+
+`
+
+const resolvers = {
+    Query: {
+        info: () => null,
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+});
